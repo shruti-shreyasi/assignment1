a0221bc9e57cdd98fd59681751e1e750f8a68a76
shruti-shreyasi <106023578+shruti-shreyasi@users.noreply.github.com>
2022-05-23 13:58:52 +0530

    cleaned code

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 import itemObjectArray from "./data.js";
 
-var PRESENTITEM = 1; //present item id 
-var LASTITEM = 0;    //id of last item
+var PRESENTITEM = 1; //present item id
+var LASTITEM = 0; //id of last item
 
 /**
- * 
- * @param {title of the image} title 
+ * limitToround specifies max length
+ * @param {title of the image} title
  * @returns description of the image
  */
 const findDescription = function (title) {
     let desc = "";
-    const limitToround = 25;
+    const limitToround = 33;
     if (title.length > limitToround) {
-        const myArray = title.split(".");
-        let dum = myArray[1];
-        let numEnd = dum.length;
-        numEnd = limitToround - 15 - numEnd;
-        numEnd = title.length - numEnd;
         desc = title.substring(0, 14) + "...";
-        desc = desc + myArray[0].substring(numEnd);
-        desc = desc + "." + dum;
+        let numEnd = limitToround - 17;
+        desc = desc + title.substring(title.length - numEnd);
     } else {
         desc = title;
     }
@@ -47,12 +42,14 @@ for (const item of itemObjectArray) {
 
 /**
  * populate grid on right
- * @param {present id} presentItem 
+ * @param {present id} presentItem
  */
 const functionRight = function (presentItem) {
     let grid2a = document.querySelector(".grid2 article");
     grid2a.innerHTML = `
-        <img src=${presentItem.previewImage} alt=${findDescription(presentItem.title)}/>
+        <img src=${presentItem.previewImage} alt=${findDescription(
+        presentItem.title
+    )}/>
         <div class="myForm">
             <div class="textDisplayed" contenteditable="true">
                 <p>${presentItem.title}</p>
@@ -64,7 +61,9 @@ const functionRight = function (presentItem) {
         let val1 = document.querySelector(".textDisplayed p").innerHTML;
         const idToBeChanged = document.getElementById(PRESENTITEM);
         idToBeChanged.innerHTML = `
-            <img src=${itemObjectArray[PRESENTITEM-1].previewImage} alt=${val1} width="30px" height="30px"/>
+            <img src=${
+                itemObjectArray[PRESENTITEM - 1].previewImage
+            } alt=${val1} width="30px" height="30px"/>
             <span>${findDescription(val1)}</span>
         `;
         itemObjectArray[PRESENTITEM - 1].title = val1;
@@ -76,39 +75,43 @@ functionRight(itemObjectArray[0]);
 /**
  * event listener for click on left nav bar
  */
-grid1.addEventListener("click", (event) => {
-    LASTITEM = PRESENTITEM;
-    if (event.path[0].hasAttribute("id")) {
-        PRESENTITEM = event.path[0].getAttribute("id");
-        functionRight(itemObjectArray[PRESENTITEM - 1]);
-        changeHighlight();
-    }
-    if (event.path[1].hasAttribute("id")) {
-        PRESENTITEM = event.path[1].getAttribute("id");
-        functionRight(itemObjectArray[PRESENTITEM - 1]);
-        changeHighlight();
-    }
-}, false);
+grid1.addEventListener(
+    "click",
+    (event) => {
+        LASTITEM = PRESENTITEM;
+        if (
+            event.path[0].hasAttribute("id") ||
+            event.path[1].hasAttribute("id")
+        ) {
+            if (event.path[0].hasAttribute("id")) {
+                PRESENTITEM = event.path[0].getAttribute("id");
+            } else {
+                PRESENTITEM = event.path[1].getAttribute("id");
+            }
+            functionRight(itemObjectArray[PRESENTITEM - 1]);
+            changeHighlight();
+        }
+    },
+    false
+);
 
 /**
  * event listener for arrow keys on left nav bar
  */
 document.addEventListener("keydown", (event) => {
-    if (event.key == "ArrowDown") {
+    if (event.key == "ArrowDown" || event.key == "ArrowUp") {
         LASTITEM = PRESENTITEM;
-        PRESENTITEM = PRESENTITEM - 1 + 2;
-        if (PRESENTITEM == (itemObjectArray.length + 1)) {
-            PRESENTITEM = 1;
-        }
-        functionRight(itemObjectArray[PRESENTITEM - 1]);
-        changeHighlight();
-    }
-    if (event.key == "ArrowUp") {
-        LASTITEM = PRESENTITEM;
-        if (PRESENTITEM == 1) {
-            PRESENTITEM = itemObjectArray.length;
+        if (event.key == "ArrowDown") {
+            PRESENTITEM = PRESENTITEM - 1 + 2;
+            if (PRESENTITEM == itemObjectArray.length + 1) {
+                PRESENTITEM = 1;
+            }
         } else {
-            PRESENTITEM = PRESENTITEM - 1;
+            if (PRESENTITEM == 1) {
+                PRESENTITEM = itemObjectArray.length;
+            } else {
+                PRESENTITEM = PRESENTITEM - 1;
+            }
         }
         functionRight(itemObjectArray[PRESENTITEM - 1]);
         changeHighlight();
@@ -120,5 +123,6 @@ const changeHighlight = function () {
     if (LASTITEM) {
         document.getElementById(LASTITEM).style.background = "white";
     }
-    document.getElementById(PRESENTITEM).style.background = "rgba(181, 229, 231, 50%)";
-}
+    document.getElementById(PRESENTITEM).style.background =
+        "rgba(181, 229, 231, 50%)";
+};
